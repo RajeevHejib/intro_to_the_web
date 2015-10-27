d3648c560c66f3652d00b53c6c829c7ce63a824c
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-27 06:21:39 +0000

    updated README.md

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'Secret route'
+end
+
+get "/random-cat" do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+post "/named-cat" do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get "/cat-form" do
+  erb :cat_form
+end
